e8e899a3624007666264cda4036c88761bf38ea9
Lyall Jonathan Di Trapani <lj.ditrapani@gmail.com>
2016-05-29 09:40:45 -0400

    Add head, tail, isEmpty methods to interface

--- list.scala
+++ list.scala
@@ -1,10 +1,20 @@
 package info.ditrapani.overview
 
 // Called List in standard library
-sealed abstract class Lst
+sealed abstract class Lst {
+  def head: Int
+  def tail: Lst
+  def isEmpty: Boolean
+}
 
 // Called :: (Cons) in standard library
-final class Cell(val head: Int, val tail: Lst) extends Lst
+final class Cell(val head: Int, val tail: Lst) extends Lst {
+  override def isEmpty = false
+}
 
 // Called Nil in standard library
-final class Empty extends Lst
+final class Empty extends Lst {
+  override def head = throw new RuntimeException("Empty.head not allowed")
+  override def tail = throw new RuntimeException("Empty.tail not allowed")
+  override def isEmpty = true
+}
--- src/test/scala/list_spec.scala
+++ src/test/scala/list_spec.scala
@@ -8,13 +8,10 @@ class ListSpec extends Spec {
 
     it("can hold elements") {
       val list: Lst = new Cell(1, new Cell(2, new Cell(3, new Empty)))
-      val c1: Cell = list.asInstanceOf[Cell]
-      c1.head should === (1)
-      val c2: Cell = c1.tail.asInstanceOf[Cell]
-      c2.head should === (2)
-      val c3: Cell = c2.tail.asInstanceOf[Cell]
-      c3.head should === (3)
-      c3.tail shouldBe an [Empty]
+      list.head should === (1)
+      list.tail.head should === (2)
+      list.tail.tail.head should === (3)
+      list.tail.tail.tail.isEmpty should === (true)
     }
   }
 }
