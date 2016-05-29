ec5445fbe3a7f47029a5242104520949407a1f42
Lyall Jonathan Di Trapani <lj.ditrapani@gmail.com>
2016-05-29 14:05:22 -0400

    Add map, size, & toString methods

--- list.scala
+++ list.scala
@@ -1,5 +1,7 @@
 package info.ditrapani.overview
 
+import scala.annotation.tailrec
+
 // Called List in standard library
 sealed abstract class Lst {
   def head: Int
@@ -7,11 +9,30 @@ sealed abstract class Lst {
   def tail: Lst
 
   def isEmpty: Boolean
+
+  def map(f: Int => Int): Lst
+
+  def size: Int = sizeIter(0)
+
+  def sizeIter(n: Int): Int
+
+  override def toString(): String = toStringIter("Lst( ")
+
+  def toStringIter(prefix: String): String
 }
 
 // Called :: (Cons) in standard library
 final class Cell(val head: Int, val tail: Lst) extends Lst {
   override def isEmpty = false
+
+  override def map(f: Int => Int) = {
+    new Cell(f(head), tail.map(f))
+  }
+
+  override def sizeIter(n: Int): Int = tail.sizeIter(n + 1)
+
+  override def toStringIter(prefix: String) =
+    tail.toStringIter(s"${prefix}${head} ")
 }
 
 // Called Nil in standard library
@@ -21,4 +42,12 @@ final class Empty extends Lst {
   override def tail = throw new RuntimeException("Empty.tail not allowed")
 
   override def isEmpty = true
+
+  override def map(f: Int => Int) = {
+    this
+  }
+
+  override def sizeIter(n: Int) = n
+
+  override def toStringIter(prefix: String): String = s"${prefix})"
 }
--- src/test/scala/list_spec.scala
+++ src/test/scala/list_spec.scala
@@ -2,17 +2,41 @@ package info.ditrapani.overview
 
 class ListSpec extends Spec {
   describe("Lst") {
+    val list: Lst = new Cell(1, new Cell(2, new Cell(3, new Empty)))
+
     it("can be empty") {
       val e: Lst = new Empty
     }
 
     it("can hold elements") {
-      val list: Lst = new Cell(1, new Cell(2, new Cell(3, new Empty)))
       list.isEmpty should === (false)
       list.head should === (1)
       list.tail.head should === (2)
       list.tail.tail.head should === (3)
       list.tail.tail.tail.isEmpty should === (true)
+    }
+
+    describe("map") {
+      it("returns empty list on empty list") {
+        (new Empty).map(_ + 5).isEmpty should === (true)
+      }
+
+      it("applies function to each element in list") {
+        val list2 = list.map(_ * 2)
+        list2.head should === (2)
+      }
+    }
+
+    describe("size") {
+      it("returns size of list") {
+        list.size should === (3)
+      }
+    }
+
+    describe("toString") {
+      it("returns a string representation of the list") {
+        list.toString should === ("Lst( 1 2 3 )")
+      }
     }
   }
 }
