63171b83eedd31fcc8f55d4693922abf3fcaf7e1
Lyall Jonathan Di Trapani <lj.ditrapani@gmail.com>
2016-05-29 09:31:36 -0400

    Lst implementation without methods

--- src/test/scala/list_spec.scala
+++ src/test/scala/list_spec.scala
@@ -1,9 +1,20 @@
 package info.ditrapani.overview
 
 class ListSpec extends Spec {
-  describe("List") {
-    it("fails") {
-      true.should(===(false))
+  describe("Lst") {
+    it("can be empty") {
+      val e: Lst = new Empty
+    }
+
+    it("can hold elements") {
+      val list: Lst = new Cell(1, new Cell(2, new Cell(3, new Empty)))
+      val c1: Cell = list.asInstanceOf[Cell]
+      c1.head should === (1)
+      val c2: Cell = c1.tail.asInstanceOf[Cell]
+      c2.head should === (2)
+      val c3: Cell = c2.tail.asInstanceOf[Cell]
+      c3.head should === (3)
+      c3.tail shouldBe an [Empty]
     }
   }
 }
