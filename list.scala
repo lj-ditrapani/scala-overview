319f45f42bd684d569975f8aaa752336b4b92a5c
Lyall Jonathan Di Trapani <lj.ditrapani@gmail.com>
2016-05-29 10:04:18 -0400

    Formatting, remove opt, add assertion

--- list.scala
+++ list.scala
@@ -3,7 +3,9 @@ package info.ditrapani.overview
 // Called List in standard library
 sealed abstract class Lst {
   def head: Int
+
   def tail: Lst
+
   def isEmpty: Boolean
 }
 
@@ -15,6 +17,8 @@ final class Cell(val head: Int, val tail: Lst) extends Lst {
 // Called Nil in standard library
 final class Empty extends Lst {
   override def head = throw new RuntimeException("Empty.head not allowed")
+
   override def tail = throw new RuntimeException("Empty.tail not allowed")
+
   override def isEmpty = true
 }
--- src/test/scala/list_spec.scala
+++ src/test/scala/list_spec.scala
@@ -8,6 +8,7 @@ class ListSpec extends Spec {
 
     it("can hold elements") {
       val list: Lst = new Cell(1, new Cell(2, new Cell(3, new Empty)))
+      list.isEmpty should === (false)
       list.head should === (1)
       list.tail.head should === (2)
       list.tail.tail.head should === (3)
