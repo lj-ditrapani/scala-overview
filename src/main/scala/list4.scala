ff2e6cef2b4118f32c9ec0f9a7d42161d19740ae
Lyall Jonathan Di Trapani <lj.ditrapani@gmail.com>
2022-06-18 23:48:30 -0400

    Make Empty an object up until step 6 part 2

--- src/main/scala/list4.scala
+++ src/main/scala/list4.scala
@@ -4,42 +4,42 @@ import scala.annotation.tailrec
 
 enum Lst:
   case Cell(head: Int, tail: Lst)
-  case Empty()
+  case Empty
 
   def isEmpty: Boolean =
     this match
-      case _: Empty => true
+      case Empty => true
       case _: Cell => false
 
-  def map(f: Int => Int): Lst = reverse.mapIter(Empty(), f)
+  def map(f: Int => Int): Lst = reverse.mapIter(Empty, f)
 
   @tailrec final def mapIter(acc: Lst, f: Int => Int): Lst =
     this match
-      case Empty() => acc
+      case Empty => acc
       case Cell(h, t) => t.mapIter(Cell(f(h), acc), f)
 
   @tailrec final def reduce[B](zero: B)(f: (B, Int) => B): B =
     this match
-      case Empty() => zero
+      case Empty => zero
       case Cell(h, t) => t.reduce(f(zero, h))(f)
 
-  def reverse: Lst = reverseIter(Empty())
+  def reverse: Lst = reverseIter(Empty)
 
   @tailrec final def reverseIter(acc: Lst): Lst =
     this match
-      case Empty() => acc
+      case Empty => acc
       case Cell(h, t) => t.reverseIter(Cell(h, acc))
 
   def size: Int = sizeIter(0)
 
   @tailrec private def sizeIter(n: Int): Int =
     this match
-      case Empty() => n
+      case Empty => n
       case Cell(_, t) => t.sizeIter(n + 1)
 
   override def toString(): String = toStringIter("Lst( ")
 
   @tailrec final def toStringIter(prefix: String): String =
     this match
-      case Empty() => s"${prefix})"
+      case Empty => s"${prefix})"
       case Cell(h, t) => t.toStringIter(s"${prefix}${h} ")
